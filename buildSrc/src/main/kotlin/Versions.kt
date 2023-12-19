812cb2ee2b9cadb0bf0f6f70056b774ddb64f8db
nagarajudbng <nagaraj.dbng@gmail.com>
2023-12-19 13:57:39 +0530

    Dependencies moved to buildSrc
    Dependencies moved to buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,20 @@
+object Versions {
+
+    const val coreKtx = "1.12.0"
+    const val kotlinBom = "1.8.0"
+    const val runtimeKtx="2.6.2"
+    const val activityCompose="1.8.2"
+    const val composeBom="2023.08.00"
+    const val junit = "4.13.2"
+    const val extJunit = "1.1.5"
+    const val espressoCore = "3.5.1"
+    const val composeBom2="2023.08.00"
+    const val testJunit="1.1.5"
+    const val material="1.11.0"
+    const val appCompact="1.6.1"
+
+    const val hiltCompiler= "1.0.0"
+    const val hiltWork= "1.0.0"
+    const val hiltAndroidCompiler= "2.48"
+    const val hiltAndroid= "2.48"
+}
